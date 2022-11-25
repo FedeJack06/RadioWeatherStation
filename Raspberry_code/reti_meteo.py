6cc6cac0e71c527e8cf69d9a51af3eb738d07751
FedeJack06 <fedejek06@gmail.com>
2022-11-25 21:48:10 +0100

    upload

--- Raspberry_code/reti_meteo.py
+++ Raspberry_code/reti_meteo.py
@@ -1,12 +1,13 @@
 import time
 import mysql.connector
 from datetime import datetime
+import smtplib, ssl
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="user",
-  password="",
-  database="db"
+  user="meteo",
+  password="federico3000",
+  database="meteo"
 )
 mycursor = mydb.cursor()
 
@@ -14,44 +15,87 @@ lineameteo = open("/home/pi/www/meteo.txt", "w")
 meteonetwork = open("/home/pi/www/pmn175.txt", "a")
 
 daysaved = int(datetime.today().strftime('%d'))
-starttime = time.time()
+timerRM = time.time()
+timerVE = time.time()
+
+direzioni = ["NNE","NE","ENE","E","ESE","SE","SSE","S","SSO","SO","OSO","O","ONO","NO","NNO","N"]
+gradi = [22,45,67,90,112,135,157,180,202,225,247,270,292,315,337,0]
+#windang = gradi[direzioni.index(winddir)]
+#winddir = direzioni[gradi.index(int(windang))]
+dooo = True
 
 while True:
 	#time date
 	todayMYSQL = datetime.today().strftime('%Y-%m-%d')
-	nowtime = datetime.today().strftime('%H:%M:%S')
+	#nowtime = datetime.today().strftime('%H:%M:%S')
 
-	#lineameteo
-	mycursor.execute(f"select tp,ur,pressionelivellodelmare/100, windMedio*3.6, windDirMedio, mmPioggia, min(tp), max(tp), max(windMax)*3.6, rainrate from dataday where date = '{todayMYSQL}'")
-	linM = mycursor.fetchone()
+	if (time.time() - timerRM) > 60:
+		timerRM = time.time()
+		print("1min")
+		mycursor.execute(f"select tp,ur,pressionelivellodelmare/100, wind*3.6, windAng, mmPioggia, rainrate, date, time, dewpoint from dataday where date = '{todayMYSQL}' ORDER BY TIMESTAMP(date,time) DESC LIMIT 1")
+		data = mycursor.fetchone()
+		mycursor.execute(f"select min(tpmin), max(tpmax), max(windmax)*3.6 from estremi where date = '{todayMYSQL}'")
+		extr = mycursor.fetchone()
+		mydb.commit()
 
-	if linM[0]:
-		lineameteo = open("/home/pi/www/meteo.txt", "w")
-		today2 = datetime.today().strftime('%d/%m/%Y')
-		lineameteo.write(today2 + " " + nowtime)
-		lineameteo.write("&v0=44.6327777777775&v0=8.6&v0=Cremolino(AL)&v0=312m s.l.m.&v0=")
-		str1 = str(linM[0]) + "&v0=" + str(linM[1]) + "&v0=" + str(linM[2]) + "&v0=" + str(linM[3]) + "&v0=" + linM[4] + "&v0=" + str(linM[5]) + "&v0=" + str(linM[6]) + "&v0=" + str(linM[7]) + "&v0=" + str(linM[8]) + "&v0=" + str(linM[9])
-		lineameteo.write(str1)
-		lineameteo.close()
+		if data:
+			#lineameteo
+			lineameteo = open("/home/pi/www/meteo.txt", "w")
+			todayLM = datetime.strptime(str(data[7]), '%Y-%m-%d').strftime('%d/%m/%Y')
+			lineameteo.write(todayLM + " " + str(data[8]))
+			lineameteo.write("&v0=44.6327777777775&v0=8.6&v0=Cremolino(AL)&v0=312m s.l.m.&v0=")
+			winddir = direzioni[gradi.index(data[4])]
+			str1 = str(data[0]) + "&v0=" + str(data[1]) + "&v0=" + str(data[2]) + "&v0=" + str(data[3]) + "&v0=" + winddir + "&v0=" + str(data[5]) + "&v0=" + str(extr[0]) + "&v0=" + str(extr[1]) + "&v0=" + str(extr[2]) + "&v0=" + str(data[6])
+			lineameteo.write(str1)
+			lineameteo.close()
 
-	#meteonetwork
-	mycursor.execute(f"select tp,pressionelivellodelmare/100, ur, windMedio*3.6, windAngMedio, max(windMax)*3.6, rainrate, mmPioggia, dewpoint from dataday where date = '{todayMYSQL}'")
-	MN = mycursor.fetchone()
-
-	if MN[0]:
-		if (time.time() - starttime) > 180:
-			starttime = time.time()
+			#meteonetwork
 			day = int(datetime.today().strftime('%d'))
-			print("3 min passed")
 			if daysaved != day:
 				meteonetwork = open("/home/pi/www/pmn175.txt", "w")
 				daysaved = day
 				print("pmn175 reset")
 			meteonetwork = open("/home/pi/www/pmn175.txt", "a")
-			today3 = datetime.today().strftime('%d/%m/%y')
-			nowtime2 = datetime.today().strftime('%H:%M')
-			str2 = "pmn175;" + today3 + ";" + nowtime2 + ";" + str(MN[0])  + ";" + str(round(MN[1], 1))
-			str3 = str2 + ";" + str(round(MN[2], 1)) + ";" + str(round(MN[3], 1)) + ";" + str(MN[4]) + ";" + str(round(MN[5], 1))  + ";" + str(round(MN[6], 1)) + ";" + str(round(MN[7], 1)) + ";" + str(round(MN[8], 1)) + ";IDEarduino;1.8.17;-99999;-99999;-99999;-99999; \n"
+			todayMN = datetime.strptime(str(data[7]), '%Y-%m-%d').strftime('%d/%m/%y')
+			timeMN = datetime.strptime(str(data[8]), '%H:%M:%S').strftime('%H:%M')
+			str2 = "pmn175;" + todayMN + ";" + timeMN + ";" + str(round(data[0], 1))  + ";" + str(round(data[2], 1))
+			str3 = str2 + ";" + str(round(data[1], 1)) + ";" + str(round(data[3], 1)) + ";" + str(data[4]) + ";" + str(round(extr[2], 1))  + ";" + str(round(data[6], 1)) + ";" + str(round(data[5], 1)) + ";" + str(round(data[9], 1)) + ";IDEarduino;1.8.17;-99999;-99999;-99999;-99999; \n"
 			meteonetwork.write(str3)
 			print("row add to pmn175.txt")
 			meteonetwork.close()
+
+		'''mycursor.execute(f"select tp,pressionelivellodelmare/100, ur, windMedio*3.6, windAngMedio, rainrate, mmPioggia, dewpoint, date, time from dataday where date = '{todayMYSQL}' ORDER BY TIMESTAMP(date,time) DESC LIMIT 1")
+		MN = mycursor.fetchone()
+		mycursor.execute(f"select max(windMax)*3.6 from dataday where date = '{todayMYSQL}'")
+		MNmaxwind = mycursor.fetchone()
+		mydb.commit()'''
+		#controllo inserimento data
+		mycursor.execute(f"SELECT UNIX_TIMESTAMP(TIMESTAMP(date,time)) FROM dataday ORDER BY TIMESTAMP(date,time) DESC LIMIT 1")
+		VE = mycursor.fetchone()
+		mydb.commit()
+
+		#print(VE)
+		#print(time.time() - VE[0])
+
+		if (time.time() - VE[0] ) > 600 and dooo:
+			dooo = False
+			#email stuff
+			port = 587  # For starttls
+			smtp_server = "smtp.gmail.com"
+			sender_email = "giacobbefederico3@gmail.com"
+			receiver_email = "fedejek06@gmail.com"
+			password = "uhhbuwqtwsfaqglo"
+			message = """\
+			Subject: Dati meteo non inseriti
+
+			Dati non inseriti da 10 minuti nel database."""
+			context = ssl.create_default_context()
+			with smtplib.SMTP(smtp_server, port) as server:
+				server.ehlo()  # Can be omitted
+				server.starttls(context=context)
+				server.ehlo()  # Can be omitted
+				server.login(sender_email, password)
+				server.sendmail(sender_email, receiver_email, message)
+			print("email sent")
+mycursor.close()
+mydb.close()
